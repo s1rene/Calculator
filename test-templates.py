c96eb7d5dec2ed2fe1630a6964222d660d070f51
s1rene <71638008+s1rene@users.noreply.github.com>
2024-05-21 17:31:37 +0300

    test-templates.py

--- test-templates.py
+++ test-templates.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 
@@ -93,3 +94,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
